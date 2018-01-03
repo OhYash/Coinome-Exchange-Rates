68c43f15159de170295a876b572d9b813e51474a
Yash Yadav <yashdimpu@gmail.com>
2018-01-03 16:06:27 +0530

    Fixed some minorbugs

--- LTC_INR_fetch.py
+++ LTC_INR_fetch.py
@@ -6,11 +6,11 @@ webpage = urllib.request.urlopen(weblink)
 websoup = BeautifulSoup(webpage, "html.parser")
 
 therow = websoup.body.nav
-therow = therow.find("div", {"class" : "nav_utility"})
+therow = therow.find("div", {"class" : "nav-utility"})
 therow = therow.find("div", {"class" : "row"})
 therow = therow.find_all("div", {"class" : "pl15"})
 
 for item in therow :
     item1 = item.a.find("span", {"class" : "small"})
-    item2 = item.a.find("span", {"class" : "last-market-share-b"})
+    item2 = item.a.find("span", {"class" : "last-market-rate-b"})
     print("%s : %s\n" % (item1.string, item2.span.string))
