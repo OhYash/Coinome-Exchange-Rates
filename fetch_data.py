b6f0af0b922fd50f57cb8c36eb9d9ecf405a3ea9
Yash Yadav <yashdimpu@gmail.com>
2018-01-03 18:21:36 +0530

    Update and rename LTC_INR_fetch.py to fetch_data.py
    Added a few comments.
    renamed LTC_INR_fetch.py to fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,15 +1,19 @@
 import urllib.request
+# Requires you to have Beautiful Soup pre-installed
 from bs4 import BeautifulSoup
-weblink = "https://www.coinome.com/exchange"
 
+# Get the webpage and apply BeautifulSoup
+weblink = "https://www.coinome.com/exchange"
 webpage = urllib.request.urlopen(weblink)
 websoup = BeautifulSoup(webpage, "html.parser")
 
+# Zoom into the data block
 therow = websoup.body.nav
 therow = therow.find("div", {"class" : "nav-utility"})
 therow = therow.find("div", {"class" : "row"})
 therow = therow.find_all("div", {"class" : "pl15"})
 
+# Print the data
 for item in therow :
     item1 = item.a.find("span", {"class" : "small"})
     item2 = item.a.find("span", {"class" : "last-market-rate-b"})
