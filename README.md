66f995c0350d44fb574e08f544c4db61f73cb2ce
Yash Yadav <yashdimpu@gmail.com>
2018-01-12 16:30:18 +0530

    Shifted from simple lists to pandas dataframe

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Coinome-Exchange-Rates
 ---
-###Web Scraper written in python 3 with BeautifulSoup4 library to catch live exchange rates from coinome
+Web Scraper written in python 3 with BeautifulSoup4 library to catch live exchange rates from coinome
 
 # Installation
 Requires Python 3 and [BeautifulSoup 4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup)
--- monitor_coinome.py
+++ monitor_coinome.py
@@ -1,8 +1,9 @@
 import urllib.request
 from bs4 import BeautifulSoup
 from datetime import datetime
+import pandas as pd
 
-def fetchData() :
+def fetchDataOnce() :
     weblink = "https://www.coinome.com/exchange"
     webpage = urllib.request.urlopen(weblink)
     websoup = BeautifulSoup(webpage, "html.parser")
@@ -13,19 +14,19 @@ def fetchData() :
     therow = therow.find_all("div", {"class" : "pl15"})
 
     datablock = []
+    datablock.append(datetime.now().date())
+    datablock.append(datetime.now().strftime('%H:%M:%S'))
     for item in therow :
         item1 = item.a.find("span", {"class" : "last-market-rate-b"})
         datablock.append(item1.span.string)
-    return datablock
+    dataframe = pd.DataFrame(data = [datablock], columns=['Date', 'Time', 'BTC/INR', 'BCH/INR', 'LTC/INR'])
+    return dataframe
+
+def printData() :
+    df = fetchDataOnce()
+    df.set_index(['Date', 'Time'], inplace=True)
+    print(df)
 
 # ::: MAIN from here :::
 
-datenow = datetime.now().date()
-print(" %s \t\t %s \t\t %s \t %s \t %s \n" %
-        ("DATE", "TIME", "BTC/INR", "BCH/INR", "LTC/INR"))
-
-while True :
-    timenow = datetime.now().strftime('%H:%M:%S')
-    data = fetchData()
-    print(" %s \t %s \t %s \t %s \t %s " %
-            (datenow, timenow, data[0], data[1], data[2]))
+printData()
