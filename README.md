4d15c2da3c38d4efeb2671e8057e2f4e808ca405
Yash Yadav <yashdimpu@gmail.com>
2018-01-12 17:54:24 +0530

    Updated README.md and minor changes in monitor_coinome.py

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # Coinome-Exchange-Rates
 ---
-Web Scraper written in python 3 with BeautifulSoup4 library to catch live exchange rates from coinome
+Web Scraper written in python 3 with BeautifulSoup4 library to catch live exchange rates from coinome.  
+Pandas library for storing and retriving data.
 
-# Installation
-Requires Python 3 and [BeautifulSoup 4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup)
+# Installation  
+Requires [Python 3](https://www.python.org/downloads/), [BeautifulSoup 4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup) and [Pandas](https://pandas.pydata.org/pandas-docs/stable/install.html#installing-from-pypi)  
+
+Install python3 in Ubuntu :  
+`
+	apt-get install python3  
+`
+
+Install BeautifulSoup 4 via pip :  
+`
+	pip install beautifulsoup4  
+`
+[or]  
+via apt-get in Ubuntu :  
+`
+	apt-get	install python3-bs4  
+`
+
+Install Pandas via pip :  
+`
+	pip install pandas  
+`
 
 ## Usage
-Clone this repository and simply run the program
+Clone this repository and run 'monitor_coinome.py'
+`
+	python monitor_coinome.py
+`
--- monitor_coinome.py
+++ monitor_coinome.py
@@ -4,8 +4,12 @@ from datetime import datetime
 import pandas as pd
 
 def fetchDataOnce() :
+    # Fetches data once.
+    # Return the datablock as a list.
+    # List Structure : DateTime, BTC/INR, BCH/INR, LTC/INR
     weblink = "https://www.coinome.com/exchange"
     webpage = urllib.request.urlopen(weblink)
+    datetimenow = datetime.now()
     websoup = BeautifulSoup(webpage, "html.parser")
 
     therow = websoup.body.nav
@@ -14,17 +18,18 @@ def fetchDataOnce() :
     therow = therow.find_all("div", {"class" : "pl15"})
 
     datablock = []
-    datablock.append(datetime.now().date())
-    datablock.append(datetime.now().strftime('%H:%M:%S'))
+    datablock.append(datetimenow)
     for item in therow :
         item1 = item.a.find("span", {"class" : "last-market-rate-b"})
         datablock.append(item1.span.string)
-    dataframe = pd.DataFrame(data = [datablock], columns=['Date', 'Time', 'BTC/INR', 'BCH/INR', 'LTC/INR'])
-    return dataframe
+    #dataframe = pd.DataFrame(data = [datablock], columns=['Date', 'Time', 'BTC/INR', 'BCH/INR', 'LTC/INR'])
+    #return dataframe
+    return datablock
 
 def printData() :
-    df = fetchDataOnce()
-    df.set_index(['Date', 'Time'], inplace=True)
+    db = fetchDataOnce()
+    df = pd.DataFrame(data = [db], columns=['Date-Time', 'BTC/INR', 'BCH/INR', 'LTC/INR'])
+    df.set_index(['Date-Time'], inplace=True)
     print(df)
 
 # ::: MAIN from here :::
