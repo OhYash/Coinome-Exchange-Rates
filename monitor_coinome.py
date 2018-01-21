026326a2fbe419042ba43bf49837e458bdf02974
Yash Yadav <yashdimpu@gmail.com>
2018-01-21 14:37:21 +0530

    Added sql functions in the program

--- monitor_coinome.py
+++ monitor_coinome.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import time
 import sqlite3
+import os
 
 global db
 global _printData
@@ -32,20 +33,48 @@ def fetchDataOnce() :
 def printData() :
     print(db[0].date(), db[0].strftime("%H:%M:%S"), db[1], db[2], db[3], db[4], sep=" \t ")
 
+def openDatabase() :
+    _filename = ("data/db_%s.db" % db[0].date())
+    # Check if database of the day exists
+    if(os.path.exists(_fileName)) : 
+        _createdb = 1
+    conn = sqlite3.connect(_fileName)
+    curr = conn.cursor()
+    # Create table in this fresh database
+    if(_createdb == 1) :    
+        curr.execute('''CREATE TABLE RATES
+        (time text, BTCINR real, BCHINR real, LTCINR real, DASHINR real)''')
+        curr.commit()
+    # Return cursor
+    return curr
+
+def storeData(curr) :
+    data = (db[0].strftime("%H:%M:%S"),
+           float(db[1].replace(',','')),
+           float(db[2].replace(',','')),
+           float(db[3].replace(',','')),
+           float(db[4].replace(',','')))
+    curr.execute("INSERT INTO RATES VALUES(?, ?, ?, ?, ?)", data)
+
+
+#   Temporary
+_printData = 1
+_storeData = 1
+#   End of temporary
+
+
 # ::: MAIN from here :::
 
-
-#    temporary
-_printData = 1
-_storeData = 0
 if(_printData == 1) :
     print("Date", "\t Time", "\t BTC/INR", "BCH/INR", "LTC/INR", "DASH\INR", sep=" \t ")
+if not os.path.exists("data") :
+    os.makedirs("data")
 
 while(True) :
     db = fetchDataOnce()
     if(_printData == 1) :
         printData()
     if(_storeData == 1) :
-        checkAndCreateDatabase()
-        storeData()
+        curr = openDatabase()
+        storeData(curr)
     time.sleep(30)
