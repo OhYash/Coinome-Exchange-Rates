40d055c5139b7fc475b06d02109dcd840a900073
Yash Yadav <yashdimpu@gmail.com>
2018-01-31 12:15:33 +0530

    Basic branch fully useful for personal usage

--- fetch_data.py
+++ fetch_data.py
@@ -2,19 +2,28 @@ import urllib.request
 # Requires you to have Beautiful Soup pre-installed
 from bs4 import BeautifulSoup
 
-# Get the webpage and apply BeautifulSoup
-weblink = "https://www.coinome.com/exchange"
-webpage = urllib.request.urlopen(weblink)
-websoup = BeautifulSoup(webpage, "html.parser")
+def getData() :
+    # Get the webpage and apply BeautifulSoup
+    weblink = "https://www.coinome.com/exchange"
+    webpage = urllib.request.urlopen(weblink)
+    websoup = BeautifulSoup(webpage, "html.parser")
 
-# Zoom into the data block
-therow = websoup.body.nav
-therow = therow.find("div", {"class" : "nav-utility"})
-therow = therow.find("div", {"class" : "row"})
-therow = therow.find_all("div", {"class" : "pl15"})
+    # Zoom into the data block
+    therow = websoup.body.nav
+    therow = therow.find("div", {"class" : "nav-utility"})
+    therow = therow.find("div", {"class" : "row"})
+    therow = therow.find_all("div", {"class" : "pl15"})
+    
+    # Box the data in a dictionary
+    data = {}
+    for item in therow :
+        item1 = item.a.find("span", {"class" : "small"})
+        item2 = item.a.find("span", {"class" : "last-market-rate-b"})
+        data[item1.string] = item2.span.string
 
-# Print the data
-for item in therow :
-    item1 = item.a.find("span", {"class" : "small"})
-    item2 = item.a.find("span", {"class" : "last-market-rate-b"})
-    print("%s : %s\n" % (item1.string, item2.span.string))
+    return data
+
+#Fetching Data once
+rates = getData()
+for thing in rates :
+    print(thing, "\t: Rs", rates[thing])
